b61df3eee42a64635b900a3ecccd58dc175636b1
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-05-22 21:34:02 +0900

    Create Emp.java

---
+++ Servlet/Emp/dto/Emp.java
@@ -0,0 +1,74 @@
+package dto;
+
+import java.sql.Date;
+
+public class Emp {
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private Date hiredate;
+	private double sal;
+	private double  comm;
+	private int deptno;
+	
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public int getMgr() {
+		return mgr;
+	}
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+	public Date getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(Date hiredate) {
+		this.hiredate = hiredate;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	public double getComm() {
+		return comm;
+	}
+	public void setComm(double comm) {
+		this.comm = comm;
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	@Override
+	public String toString() {
+		return "Emp [empno=" + empno + ", ename=" + ename + ", job=" + job + ", mgr=" + mgr + ", hiredate=" + hiredate
+				+ ", sal=" + sal + ", comm=" + comm + ", deptno=" + deptno + ", getEmpno()=" + getEmpno()
+				+ ", getEname()=" + getEname() + ", getJob()=" + getJob() + ", getMgr()=" + getMgr()
+				+ ", getHiredate()=" + getHiredate() + ", getSal()=" + getSal() + ", getComm()=" + getComm()
+				+ ", getDeptno()=" + getDeptno() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+	
+}
