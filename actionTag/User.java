1e6aec40436e9cdd60fbf08e78647d04c2e896b2
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-05-28 21:16:29 +0900

    #jsp예시에 이용할 DTO

---
+++ actionTag/User.java
@@ -0,0 +1,23 @@
+package dto;
+
+public class User {
+	private String id;
+	private String pw;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", pw=" + pw + "]";
+	}
+	
+}
