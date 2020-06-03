5cfe0cca5b226194af14df21d18943c9cd87ca76
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-06-03 21:42:59 +0900

    Update FileService.java

--- Fileupload/service/face/FileService.java
+++ Fileupload/service/face/FileService.java
@@ -1,7 +1,12 @@
 package service.face;
 
+import java.sql.Connection;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dto.UploadFile;
 
 public interface FileService {
 
@@ -13,4 +18,14 @@ public interface FileService {
 	 */
 	public void fileupload(HttpServletRequest req, HttpServletResponse resp);
 		
+	public List<UploadFile> list();
+	/**
+	    * 업로드된 파일 정보 기록하기
+	    * 
+	    * @param up - 업로드된 파일의 정보
+	    * @return int - DB에 insert수행한 결과
+	    *    0 - insert실패
+	    *  1 - insert성공
+	    */
+	   public int insertFile(UploadFile up);
 }
